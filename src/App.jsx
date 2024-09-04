957e01a9878897e7bf12dbaf9fc0162f02fb79de
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-04 17:06:36 +0400

    initializing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Box } from '@mui/material'
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import VideoDetail from './components/VideoDetail'
+import ChannelDetail from './components/ChannelDetail'
+import SearchFeed from './components/SearchFeed'
 
 const App = () => (
   <Router>
