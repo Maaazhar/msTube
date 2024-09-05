f26caae2950c1a2dbe9ba9a058a886ddfae8241f
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-05 21:28:04 +0400

    updating SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { IconButton, Paper } from "@mui/material"
-import { SearchIcon } from "@mui/icons-material"
+import { Search } from "@mui/icons-material"
 
 const SearchBar = () => {
   return (
-    <div>
-      SearchBar
-    </div>
+    <Paper
+      component="form"
+      onSubmit={() => { }}
+      sx={{
+        border: "1px solid #e3e3e3",
+        borderRadius: 20,
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 }
+      }}
+    >
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => { }}
+      />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+      >
+        <Search />
+      </IconButton>
+    </Paper>
   )
 }
 
