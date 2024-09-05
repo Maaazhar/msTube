34e1ea3941afcb6ce416ab71c3d29cd41a976ce8
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-05 21:39:01 +0400

    updating Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+import { Box, Stack, Typography } from "@mui/material"
 
 const Feed = () => {
+
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{ flexDirection: { sx: "column", md: "row" } }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d3",
+          px: { sx: 0, md: 2 }
+        }} >
+
+      </Box>
+    </Stack>
   )
 }
 
