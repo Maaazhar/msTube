f554ab3415ac986fa391f787e0ffbebb9ddf1cab
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-06 22:42:22 +0400

    initializing Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,15 +5,35 @@ const Feed = () => {
 
   return (
     <Stack
-      sx={{ flexDirection: { sx: "column", md: "row" } }}
-    >
+      sx={{
+        flexDirection: {
+          sx: "column",
+          md: "row"
+        }
+      }} >
       <Box
         sx={{
-          height: { sx: "auto", md: "92vh" },
-          borderRight: "1px solid #3d3d3d3",
-          px: { sx: 0, md: 2 }
+          height: {
+            sx: "auto",
+            md: "92vh"
+          },
+          borderRight: "1px solid #3d3d3d",
+          px: {
+            sx: 0,
+            md: 2
+          }
         }} >
+        Sidebar
 
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{
+            mt: 1.5,
+            color: "#fff"
+          }}>
+          Copyright 2024 @ MS Tube
+        </Typography>
       </Box>
     </Stack>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material"
 import { Link } from "react-router-dom"
-import { logo } from "../utils/constants"
+// import { logo } from "../utils/constants"
 import { SearchBar } from "."
 
 const Navbar = () => (
