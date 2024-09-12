53605e8e7258dcfb97892074a5b507925076c4b8
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-12 21:32:03 +0400

    updating SearchFeed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+import { Box, Typography } from "@mui/material"
+import { Videos } from "./"
+import { fetchFromAPI } from "../utils/fetchFromAPI"
+import { useParams } from "react-router-dom"
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
+    <Box
+      p={2}
+      sx={{
+        height: "90vh",
+        flex: 2,
+        overflowY: "auto"
+      }}>
+      <Typography
+        variant="h4"
+        fontWeight="bold" mb={2}
+        sx={{
+          color: "#fff"
+        }}>
+        Search result for: <span style={{
+          color: "#f31503"
+        }}>{searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
