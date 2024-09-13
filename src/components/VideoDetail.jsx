772068c694f42bd812af64a76a9ebbd8808c328e
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-13 18:01:03 +0400

    updating VideoDetails

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -7,11 +7,37 @@ import Videos from "./Videos"
 import { fetchFromAPI } from "../utils/fetchFromAPI"
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]))
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items))
+  }, [id]);
+
+  // if(!videoDetail?.snippet) return <Loader />;
+
+  // const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
+
+  
   return (
-    <div>
-
-    </div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{
+            width: "100%",
+            position: "sticky",
+            top: "86px"
+          }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
