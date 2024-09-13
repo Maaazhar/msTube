543d518f8aca2eee836943630ac50d40257b0bfb
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-13 17:48:05 +0400

    initializing VideoDetails

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react"
+import ReactPlayer from "react-player"
+import { Link, useParams } from "react-router-dom"
+import { CheckCircle } from "@mui/icons-material"
+import { Box, Stack, Typography } from "@mui/material"
+import Videos from "./Videos"
+import { fetchFromAPI } from "../utils/fetchFromAPI"
 
 const VideoDetail = () => {
+
   return (
-    <div>VideoDetail</div>
+    <div>
+
+    </div>
   )
 }
 
